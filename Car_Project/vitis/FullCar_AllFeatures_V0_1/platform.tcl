cd67ccbefd0e6d3be023309cacb5b2e0cdc77730
OliwierJaworski <114076196+OliwierJaworski@users.noreply.github.com>
2025-05-23 19:24:15 +0200

    C < Cpp project template >

---
+++ Car_Project/vitis/FullCar_AllFeatures_V0_1/platform.tcl
@@ -0,0 +1,25 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct M:\projects\clones\SOC\Car_Project\vitis\FullCar_AllFeatures_V0_1\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source M:\projects\clones\SOC\Car_Project\vitis\FullCar_AllFeatures_V0_1\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {FullCar_AllFeatures_V0_1}\
+-hw {M:\projects\clones\SOC\Car_Project\vivado\XSA\FullCar_AllFeatures_V0_1.xsa}\
+-out {M:/projects/clones/SOC/Car_Project/vitis}
+
+platform write
+domain create -name {standalone_ps7_cortexa9_0} -display-name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {hello_world}
+platform generate -domains 
+platform active {FullCar_AllFeatures_V0_1}
+domain active {zynq_fsbl}
+domain active {standalone_ps7_cortexa9_0}
+platform generate -quick
+platform generate
+platform generate -domains standalone_ps7_cortexa9_0,zynq_fsbl 
+platform clean
+platform generate
